89144c91cd291ca072e7b48ab0a328d4213a57e4
masumbillah360 <masumbillah6584@gmail.com>
2024-04-30 12:35:07 -0700

    updated code

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "landingfoliocom.imgix.net"
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "landingfoliocom.imgix.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
