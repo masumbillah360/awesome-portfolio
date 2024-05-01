51eec072171f4982f90ccb78634ebfd99274a6e6
masumbillah360 <masumbillah6584@gmail.com>
2024-05-01 11:46:53 +0600

    fixed theme toggling button

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,18 +1,21 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
   return (
-    <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")} variant="outline" size="icon" className="rounded-full">
+    <Button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      variant="outline"
+      size="icon"
+      className="rounded-full"
+    >
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-white" />
     </Button>
-
-  )
+  );
 }
